8d90829f4d90a7c01eb01ee53146ae2f6c996b09
Nikola Savic <snikolaa998@gmail.com>
2021-10-15 14:31:15 +0200

    Data Access Object added

---
+++ app/src/main/java/com/example/searchingnews/db/ArticleDao.kt
@@ -0,0 +1,18 @@
+package com.example.searchingnews.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.searchingnews.Article
+
+@Dao
+interface ArticleDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsertArticle(article: Article): Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles(): LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
