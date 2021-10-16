15bcde849ba0bc03fbe7c59a2930c36caea8223a
Nikola Savic <snikolaa998@gmail.com>
2021-10-16 02:02:29 +0200

    getBreakingResponse added

--- app/src/main/java/com/example/searchingnews/repository/NewsRepository.kt
+++ app/src/main/java/com/example/searchingnews/repository/NewsRepository.kt
@@ -1,6 +1,9 @@
 package com.example.searchingnews.repository
 
+import com.example.searchingnews.api.RetrofitInstance
 import com.example.searchingnews.db.ArticleDatabase
 
 class NewsRepository(val db: ArticleDatabase) {
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
 }
